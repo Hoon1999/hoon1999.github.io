3cecd67fda891a40f3ffca374dd4d93d853d0004
Hoon1999 <kjhoon44@gmail.com>
2023-07-14 12:02:10 +0900

    body tag 값 수정, sidebarUtil.isExpanded 수정

--- _javascript/modules/components/sidebar.js
+++ _javascript/modules/components/sidebar.js
@@ -2,11 +2,11 @@
  * Expand or close the sidebar in mobile screens.
  */
 
-const $body = $('body');
+const $body = $('body sidebar-display');
 const ATTR_DISPLAY = 'sidebar-display';
 
 class SidebarUtil {
-  static isExpanded = false;
+  static isExpanded = true;
 
   static toggle() {
     if (SidebarUtil.isExpanded === false) {
